01a27e1410a8cbb1d53c2a1b6b7f5778ee4eedd4
JoaoVitorDomingos <domingosjoaovitor81@gmail.com>
2026-04-10 15:12:28 -0300

    feat (escolha): implementa metodo de escolha de operacao

--- src/genetico.py
+++ src/genetico.py
@@ -1,5 +1,10 @@
 # Arquivo contendo todos os metodos relacionados ao algoritmo genetico, como a funcao de fitness, selecao, crossover e mutacao
 
+import random
+from src.enum.operacao import Operacao
+from src.const.probabilidades import OP_PROB
+
+
 def calcula_fitness(individuo):
     # Calcula a fitness do individuo
 
@@ -9,28 +14,44 @@ def calcula_fitness(individuo):
 
     return fitness
 
+
+def escolha_operacao():
+    # Escolhe a operacao a ser realizada com base nas probabilidades definidas
+
+    num = random.random()
+
+    if num < OP_PROB[Operacao.CROSSOVER]:
+        return Operacao.CROSSOVER
+    elif num < OP_PROB[Operacao.CROSSOVER] + OP_PROB[Operacao.REPRODUCAO]:
+        return Operacao.REPRODUCAO
+    else:
+        return Operacao.MUTACAO
+
+
 def selecao_roleta(populacao):
     # Realiza a selecao por roleta
     return None
 
-def escolha_operacao():
-    return None
 
 def crossover(individuo1, individuo2):
     # Realiza o crossover entre dois individuos
     return None
 
+
 def reproducao(individuo):
     # Realiza a reproducao da populacao
     return None
+
 
 def mutacao(individuo):    
     # Realiza a mutacao do individuo
     return None
 
+
 def criterio_parada(populacao):
     # Verifica se o criterio de parada foi atingido
     return None
+
 
 def substituicao_geracao():
     return None
