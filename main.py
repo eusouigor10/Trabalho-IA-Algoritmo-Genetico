850d6beb184df0b079f07c01aa710dc4847b7734
Igor Daré <eusouigor10@gmail.com>
2026-04-15 09:13:19 -0300

    estrutura do main

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ import src.genetico
 
 if __name__ == "__main__":
     print("Iniciando o algoritmo genético...")
+
+#inicialização com a criação da população inicial, caminhos iniciais e distâncias
+
+#cálculo da fitness de cada caminho
+
+#seleção e envio para crossover, mutação ou reprodução
+
+#estabelecimento da nova população
+
+#avaliação do critério de parada ou repetição do processo
