1ba606b15f4672cf1ad34fdba5ef018de5a420c3
JoaoVitorDomingos <domingosjoaovitor81@gmail.com>
2026-04-23 00:17:39 -0300

    feat (grafico): implementa metodos de gerar graficos

--- src/grafico.py
+++ src/grafico.py
@@ -1,1 +1,32 @@
 # Arquivo contendo a biblioteca de graficos e seus metodos
+
+import matplotlib.pyplot as plt
+
+import matplotlib.pyplot as plt
+
+def plotar_evolucao(valores, titulo="Evolução", ylabel="Valor"):
+    plt.figure(figsize=(8, 5))
+    
+    plt.plot(valores, marker='o')
+    
+    plt.title(titulo)
+    plt.xlabel("Geração")
+    plt.ylabel(ylabel)
+    
+    plt.grid(True)
+    
+    plt.show()
+
+
+def plotar_tempo_execucao(tamanhos, tempos):
+    plt.figure(figsize=(8, 5))
+    
+    plt.plot(tamanhos, tempos, marker='o')
+    
+    plt.title("Tempo de Execução vs Tamanho do TSP")
+    plt.xlabel("Número de Cidades")
+    plt.ylabel("Tempo de Execução (segundos)")
+    
+    plt.grid(True)
+    
+    plt.show()
